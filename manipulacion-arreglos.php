aa4840041230f3b3de373794e918b6f855400700
Daniel Ávila <daniel38avila@hotmail.com>
2025-08-11 14:01:43 -0500

    manipulando Arreglos

---
+++ manipulacion-arreglos.php
@@ -0,0 +1,29 @@
+<?php
+
+$edades = [18, 22, 19, 21];
+
+//count
+echo count($edades) . "\n";
+
+//array_push
+array_push($edades, 23);
+var_dump($edades);
+
+// is_array
+$esto_no_es_un_arreglo = "";
+var_dump(is_array($esto_no_es_un_arreglo));// false
+var_dump(is_array($edades));// true
+
+// Explode
+$lista_de_frutas = "fresa, manzana, plátano, kiwi";
+$lista_de_frutas_array = explode(", ", $lista_de_frutas);
+var_dump($lista_de_frutas_array);
+
+//inplode
+$frutas_string_array2 = ["fresa", "manzana", "plátano", "kiwi"];
+$lista_de_frutas = implode(", ", $frutas_string_array2);
+var_dump($lista_de_frutas);
+
+
+echo "\n";
+
