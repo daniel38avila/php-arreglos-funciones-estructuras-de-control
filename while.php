cb9cdab5ac0e4b4b4501f65229050cda20664068
Daniel Ávila <daniel38avila@hotmail.com>
2025-08-14 13:51:37 -0500

    ciclo while

---
+++ while.php
@@ -0,0 +1,8 @@
+$contador = 1; // Paso 1: inicializas el contador
+
+while ($contador <= 10) { // Paso 2: revisas la condición
+  echo "Hello, World! $contador \n"; // Paso 3: imprimes
+  $contador++; // Paso 4: incrementas para acercarte al fin del ciclo
+}
+
+echo PHP_EOL; // Esto solo agrega un salto de línea final
